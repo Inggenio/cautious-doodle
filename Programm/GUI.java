bba1954dc23cd28b91b46c0565abdfa78f071fbf
inggenio <inggenio13@gmail.com>
2025-04-03 23:42:08 +0200

    Initial Class Commit

--- Programm/GUI.java
+++ Programm/GUI.java
@@ -1,4 +1,25 @@
 package Programm;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class GUI {
+	JFrame myWindow = new JFrame();
+	JPanel chalkboard = new JPanel();
+
+	BorderLayout bLayout = new BorderLayout();
+	JButton openBtn = new JButton();
+
+	JLabel lbl1 = new JLabel();
+
+	public void go(){
+
+		chalkboard.add(openBtn);
+		chalkboard.setLayout(bLayout);
+
+
+		myWindow.setSize(new Dimension(500,500));
+		myWindow.setVisible(true);
+		myWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
 }
--- Programm/Main.java
+++ Programm/Main.java
@@ -2,6 +2,7 @@ package Programm;
 
 public class Main {
 	public static void main(String[] args) {
-
+		GUI run = new GUI();
+		run.go();
 	}
 }
